1c5a9b655e5c2b9cb8c3f89ab041253d7aa0fbdc
ACER <ACER@DESKTOP-U4RVC4E>
2017-08-24 17:57:53 +0800

    0824 QQQQQQQ

--- src/com/gyms/controller/GymsServlet.java
+++ src/com/gyms/controller/GymsServlet.java
@@ -1,6 +1,7 @@
 package com.gyms.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Base64;
 import java.util.Base64.Encoder;
 import java.util.Date;
@@ -21,6 +22,7 @@ import javax.servlet.http.Part;
 
 import com.coaches.model.CoachesService;
 import com.coaches.model.CoachesVO;
+import com.google.gson.Gson;
 import com.gyms.model.GymsService;
 import com.gyms.model.GymsVO;
 import com.members.model.MembersService;
@@ -36,6 +38,8 @@ public class GymsServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		String action = req.getParameter("action");
+		PrintWriter out = res.getWriter();
+		
 		if(action == null) {
 			System.out.println("action == null");
 			String url = "/front_end/index.jsp";
@@ -253,14 +257,6 @@ public class GymsServlet extends HttpServlet {
 				}
 			}
 			
-			// 來自index.jsp的logout請求
-			if ("logout".equals(action)) {
-				HttpSession session = req.getSession();
-				session.invalidate();
-				res.sendRedirect(req.getContextPath()+"/front_end/index.jsp");
-			    return;
-			}
-			
 			//來自personal.jsp的請求
 			if ("update".equals(action)) { 
 				Map<String,String> errorMsgs = new LinkedHashMap<String,String>();
@@ -312,51 +308,21 @@ public class GymsServlet extends HttpServlet {
 					}else if(gym_into.length() > 500) {
 						errorMsgs.put("gym_into", "會員自我介紹: 格式錯誤");
 					}
-					
-//					//驗證大頭貼
-//					String cropped_pic = req.getParameter("cropped_pic");
-//					//base64轉byte[]
-//					Base64.Decoder decoder = Base64.getDecoder();
-//					byte[] coa_pic_byte = null;
-//					
-//					if(cropped_pic == null || "".equals(cropped_pic)) {
-//						errorMsgs.put("coa_pic", "會員大頭貼: 請鎖定圖片");
-//					}else {
-//						coa_pic_byte = decoder.decode(cropped_pic.split(",")[1]);
-//						//圖片大小(kb...)
-//						int pic_length = coa_pic_byte.length;
-//						String pic_type = cropped_pic.substring(5, 10);
-////						Part coa_pic = req.getPart("upload-file");
-////						String fileType = coa_pic.getContentType();
-//						if(pic_length > (5*1024*1024)) {
-//							errorMsgs.put("coa_pic", "會員大頭貼: 檔案過大");
-//						} else if(!("image".equals(pic_type))) {
-//							errorMsgs.put("coa_pic", "會員大頭貼: 僅允許圖片格式");
-//						}
-//					}
-					
-//					System.out.println("PartSize=" + coa_pic.getSize());
-//					System.out.println("I got the parameter from form");
-					
+			
 					// 資料有誤就返回form表單
 					if(!errorMsgs.isEmpty()) {
-						System.out.println("I got the errorMsgs");
-						String url = "/front_end/editPage/personal.jsp";
-						RequestDispatcher failureView = req.getRequestDispatcher(url);
-						failureView.forward(req, res);
+						Gson gSon = new Gson();
+						out.print(gSon.toJson(errorMsgs));
 						return;
-					}				
+					}		
 					//2.開始修改資料
 					GymsService gymsSV = new GymsService();
 					gymsVO = gymsSV.updateGyms(gymsVO, gym_name, gym_mail, gym_into, gym_add, gym_latlng);
 					gymsVO = gymsSV.loginGyms(gymsVO.getGym_acc(), gymsVO.getGym_psw());
 					//3.修改完成,準備轉交(Send the Success view)
-					System.out.println("update is complete");
 					session.setAttribute("user", membersVO);
 					session.setAttribute("gym", gymsVO);
-					String url = req.getContextPath() + "/front_end/editPage/personal.jsp";
-					System.out.println("ok!");
-					res.sendRedirect(url);
+					System.out.println("update is complete");
 					return;
 					
 				//其他可能的錯誤處理
